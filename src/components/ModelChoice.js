8a019156314d748b2e6067bcf3bd9d45a10b978f
raouf-mediaplus <raouf.benabdelwahed@gmail.com>
2024-03-08 20:28:35 +0100

    lastpoint

--- src/components/ModelChoice.js
+++ src/components/ModelChoice.js
@@ -1,20 +1,52 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@mui/material";
+import Divider from "@mui/material/Divider";
+import "./ModelChoice.css";
 
 var modelsData = require("../data/models.json");
 
 function ModelChoice() {
   const [models, setModels] = useState([]);
-  const [categories, setCategories] = useState([]);
-
-  // useEffect(() => {
-  //   setCategories(modelsData);
-  // }, []);
+  const [selected, setSelected] = useState(modelsData[0].id);
+  const [checkedCategories, setCheckedCategories] = useState({});
+  const [checkedMetrics, setCheckedMetrics] = useState({});
 
   useEffect(() => {
     setModels(modelsData);
   }, []);
+  useEffect(() => {
+    const initialCheckedCategories = {};
+    const initialCheckedMetrics = {};
+
+    // Initialise les états des catégories à cocher
+    modelsData[selected].Categories.forEach((cat) => {
+      initialCheckedCategories[cat.Category] = true;
+    });
+
+    // Initialise les états des métriques à cocher
+    modelsData[selected].Categories.forEach((cat) => {
+      cat.Values.forEach((metric) => {
+        initialCheckedMetrics[metric.Metric] = false;
+      });
+    });
+
+    setCheckedCategories(initialCheckedCategories);
+    setCheckedMetrics(initialCheckedMetrics);
+  }, [selected]);
+
+  const handleSelect = (event) => {
+    setSelected(event.target.value);
+  };
+  const handleCategoryChange = (event) => {
+    const { name, checked } = event.target;
+    setCheckedCategories({ ...checkedCategories, [name]: checked });
+  };
+
+  const handleMetricChange = (event) => {
+    const { name, checked } = event.target;
+    setCheckedMetrics({ ...checkedMetrics, [name]: checked });
+  };
 
   const optionsSelect = models.map((model) => (
     <option key={model.id} value={model.id}>
@@ -22,57 +54,45 @@ function ModelChoice() {
     </option>
   ));
 
-  const [selected, setSelected] = useState(modelsData[0].id);
-  const handleSelect = (event) => {
-    // console.log(event.target.value);
-    setSelected(event.target.value);
-  };
-
   const CategoriesList = modelsData[selected].Categories.map((cat) => (
-    <>
-      <dd key={cat.id}>
-        <input type="checkbox" name={cat.Category} value={cat.Category} />
-        {cat.Category}
-      </dd>
+    <div className="categories-list-item" key={cat.id}>
+      <input
+        style={{ marginBottom: "1em", marginTop: "1em" }}
+        type="checkbox"
+        checked={checkedCategories[cat.Category] || true}
+        name={cat.Category}
+        value={cat.Category}
+        onChange={handleCategoryChange}
+      />
+      <span>{cat.Category}</span>
       {cat.Values.map((metric) => (
-        <dt key={metric.id}>
-          <input type="checkbox" name={metric.Metric} value={metric.Metric} />
-          {metric.Metric}
-        </dt>
+        <div key={metric.id}>
+          <input
+            style={{ marginLeft: "2em", marginBottom: "1em" }}
+            type="checkbox"
+            checked={checkedMetrics[metric.Metric] || true}
+            name={metric.Metric}
+            value={metric.Metric}
+            onChange={handleMetricChange}
+          />
+          <span>{metric.Metric}</span>
+        </div>
       ))}
-    </>
+    </div>
   ));
 
   return (
-    <div>
-      <p
-        style={{
-          fontSize: "2em",
-          fontWeight: "bold",
-          marginTop: "0.8rem",
-          marginBottom: "0",
-          color: "white",
-        }}
-      >
-        Choose your metrics
+    <div className="model-choice-container">
+      <h1 className="model-choice-header">Choose your metrics</h1>
+      <p className="model-choice-description">
+        Here you can choose your performance test system and the metrics you
+        want to focus on.
       </p>
-      <p
-        style={{
-          fontSize: "1em",
-          marginTop: 0,
-          marginBottom: 0,
-          opacity: "0.7",
-          color: "white",
-        }}
-      >
-        This quiz will allow you to assess your DevOps performance based on many
-        metrics.
-      </p>
-
-      <select name="models" onChange={handleSelect}>
+      <select className="model-select" name="models" onChange={handleSelect}>
         {optionsSelect}
       </select>
-      <ul style={{ color: "white" }}>{CategoriesList}</ul>
+      <Divider style={{ borderColor: "rgba(255,255,255,0.1)" }} />
+      <ul className="categories-list">{CategoriesList}</ul>
       <Link to="/quiz">
         <Button variant="contained" color="primary" className="login-button">
           Start the Quiz
